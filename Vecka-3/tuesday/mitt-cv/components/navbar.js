df598d570a19ee9a7087f6afad22706725b9f8e7
joeljanson <joel.janson.johansen@gmail.com>
2025-12-16 11:33:59 +0100

    implemented vue-component for navbar

---
+++ Vecka-3/tuesday/mitt-cv/components/navbar.js
@@ -0,0 +1,12 @@
+const NavBar = {
+	template: `
+    <nav>
+		<div class="logo">Joel Janson Johansen</div>
+		<ul class="nav-links">
+			<li><a href="index.html">Hem</a></li>
+			<li><a href="about.html">Om</a></li>
+			<li><a href="contact.html">Kontakt</a></li>
+		</ul>
+	</nav>
+  `,
+};
